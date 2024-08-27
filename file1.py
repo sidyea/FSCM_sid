050c1e44807108ab668bbf4e0edc96f44efbcb80
sidyea <siddhant.dhanrajani@protonmail.com>
2024-08-27 13:04:50 +0200

    added sobel filter function

--- file1.py
+++ file1.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from scipy import ndimage
+# https://towardsdatascience.com/canny-edge-detection-step-by-step-in-python-computer-vision-b49c3a2d8123
 
 def gaussian_kernel(size, sigma=1):
     size = int(size) // 2
@@ -7,4 +8,18 @@ def gaussian_kernel(size, sigma=1):
     normal = 1 / (2.0 * np.pi * sigma**2)
     g = np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
     return g
+
+
+def sobel_filters(img):
+    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+
+    Ix = ndimage.filters.convolve(img, Kx)
+    Iy = ndimage.filters.convolve(img, Ky)
+
+    G = np.hypot(Ix, Iy)
+    G = G / G.max() * 255
+    theta = np.arctan2(Iy, Ix)
+
+    return G, theta
 
